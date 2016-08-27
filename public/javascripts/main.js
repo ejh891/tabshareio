ed5111579c525a33149f7e11d4389eb1e2a8d431
EJ Hammond <ejh891@gmail.com>
2016-08-27 10:48:45 -0400

    Move non-dom-reading functions outside of document.ready

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,30 @@
+var validateCurrency = function ($inputElem) {
+    var numStr = $inputElem.val();
+    if (validateNumber($inputElem) !== false)
+        return (+numStr).toFixed(2);
+    return false;
+};
+
+var validateNumber = function ($inputElem) {
+    var numStr = $inputElem.val();
+    var numNum = +numStr;
+    if (isNaN(numNum) || numNum === 0)
+        return false;
+    return numNum;
+};
+
+var splitPossible = function (totalAmount, numPeople) {
+    return totalAmount * 100 > numPeople;
+};
+
+var displaySplitCost = function (num) {
+    $("#split-cost").text(num.toFixed(2));
+};
+
+var displayExchangeRate = function (num) {
+    $("#exchange-rate-value").text(num);
+};
+
 $(document).ready(function() {
     var exchangeRate;
 
@@ -14,33 +41,6 @@ $(document).ready(function() {
                 displayExchangeRate(exchangeRate);
             });
     }();
-
-    var validateCurrency = function ($inputElem) {
-        var numStr = $inputElem.val();
-        if (validateNumber($inputElem) !== false)
-            return (+numStr).toFixed(2);
-        return false;
-    };
-
-    var validateNumber = function ($inputElem) {
-        var numStr = $inputElem.val();
-        var numNum = +numStr;
-        if (isNaN(numNum) || numNum === 0)
-            return false;
-        return numNum;
-    };
-
-    var splitPossible = function (totalAmount, numPeople) {
-        return totalAmount * 100 > numPeople;
-    };
-
-    var displaySplitCost = function (num) {
-        $("#split-cost").text(num.toFixed(2));
-    };
-
-    var displayExchangeRate = function (num) {
-        $("#exchange-rate-value").text(exchangeRate);
-    };
 
     var calculateSplit = function () {
         var totalAmountCad = validateCurrency($("input[name=total_amount_cad]"));
