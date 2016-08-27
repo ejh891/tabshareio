daedbbe91d49d7732f6995c925bf2b74b421bb6d
EJ Hammond <ejh891@gmail.com>
2016-08-27 10:26:01 -0400

    Replace deprecated ajax callback chain functions

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,14 +1,19 @@
 $(document).ready(function() {
-    var exchangeRate = 0.77;
+    var exchangeRate;
 
     var getExchangeRate = function () {
-        $.get( "https://api.fixer.io/latest?base=CAD&symbols=CAD,USD", function(data) {
-            exchangeRate = data.rates.USD || 0.77;
-            displayExchangeRate(exchangeRate);
-        });
-    };
-
-    var exchangeRate = getExchangeRate();
+        $.get("https://api.fixer.io/latest?base=CAD&symbols=CAD,USD")
+            .done(function(data) {
+                exchangeRate = data.rates.USD;
+            })
+            .fail(function(data, textStatus, errorThrown) {
+                exchangeRate = 0.77;
+                console.log(textStatus);
+            })
+            .always(function() {
+                displayExchangeRate(exchangeRate);
+            });
+    }();
 
     var validateCurrency = function ($inputElem) {
         var numStr = $inputElem.val();
