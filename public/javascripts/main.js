811b60b5da662be778382e813a305e5d29397f0e
EJ Hammond <ejh891@gmail.com>
2016-08-27 01:32:36 -0400

    Get real exchange rate via ajax; display exchange rate on page

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
+    var exchangeRate = 0.77;
+
+    var getExchangeRate = function () {
+        jQuery.get( "http://api.fixer.io/latest?base=CAD&symbols=CAD,USD", function(data) {
+            exchangeRate = data.rates.USD || 0.77;
+            displayExchangeRate(exchangeRate);
+        });
+    };
+
+    var exchangeRate = getExchangeRate();
+
     var validateCurrency = function ($inputElem) {
         var numStr = $inputElem.val();
         if (validateNumber($inputElem) !== false)
@@ -22,6 +33,10 @@ $(document).ready(function() {
         $("#split-cost").text(num.toFixed(2));
     };
 
+    var displayExchangeRate = function (num) {
+        $("#exchange-rate-value").text(exchangeRate);
+    };
+
     var calculateSplit = function () {
         var totalAmountCad = validateCurrency($("input[name=total_amount_cad]"));
         var numPeople = validateNumber($("select[name=num_people]"));
@@ -32,7 +47,7 @@ $(document).ready(function() {
         }
 
         var costPerPersonCad = totalAmountCad / numPeople;
-        var costPerPersonUsd = costPerPersonCad * 0.77;
+        var costPerPersonUsd = costPerPersonCad * exchangeRate;
 
         displaySplitCost(costPerPersonUsd);
     };
